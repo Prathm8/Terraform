9ac409df9b55de0857a8d13d369d0c7983fb2959
Prathm8 <159471585+Prathm8@users.noreply.github.com>
2024-04-13 20:54:08 +0530

    Update main.tf

--- project-eks/main.tf
+++ project-eks/main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 locals {
   name   = "nareshit-cluster"
-  region = "us-east-1"
+  region = "us-west-1"
 
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
+  azs      = ["us-west-1a", "us-west-1b"]
 
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
